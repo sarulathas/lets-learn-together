a2105d2842a9c214c223edc79458e35c40f62b5b
Sarulatha Shanmugam <sarulathas@sahaj.ai>
2025-02-26 21:58:33 +1100

    Updating README and adding tests

---
+++ MelbourneCityLibrary/tests/LibraryServiceTest.java
@@ -0,0 +1,56 @@
+package tests;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import domainObjects.Book;
+import domainObjects.User;
+import services.LibraryService;
+
+public class LibraryServiceTest {
+
+    private LibraryService service = new LibraryService();
+
+    @Test
+    public void testAddNewBook() {
+        Book newBook = new Book("1", "New Book", "Author Name");
+
+        service.addNewBook(newBook);
+
+        Assertions.assertEquals(service.getBooks().get(newBook.getBookId()), newBook);
+    }
+
+    @Test
+    public void testAddNewUser() {
+        User newUser = new User("1", "User Name");
+
+        service.addNewUser(newUser);
+
+        Assertions.assertEquals(service.getUsers().get(newUser.getUserId()), newUser);
+    }
+
+    @Test
+    public void testBorrowBook() {
+        Book book = new Book("1", "New Book", "Author Name");
+        User user = new User("1", "User Name");
+        service.addNewBook(book);
+        service.addNewUser(user);
+
+        service.borrowBook("1", "1");
+
+        Assertions.assertEquals(service.getBooks().get(book.getBookId()).isBookAvailable(), false);
+    }
+
+    @Test
+    public void testReturnBook() {
+        Book book = new Book("1", "New Book", "Author Name");
+        User user = new User("1", "User Name");
+        service.addNewBook(book);
+        service.addNewUser(user);
+        service.borrowBook("1", "1");
+
+        service.returnBook("1");
+
+        Assertions.assertEquals(service.getBooks().get(book.getBookId()).isBookAvailable(), true);
+    }
+}
--- README.md
+++ README.md
@@ -2,22 +2,31 @@
 
 Repository for Let's Learn Together Work
 
-Done so far:
+### Structure
 
-- Classes created for Book, User and MelbourneCityLibrary
-- A console application created with functionality to add new user and books, list, borrow and return books.
+Melbourne City Library
 
-To be done:
+- domainObjects
+  - Book.java
+  - Transaction.java
+  - User.java
+- services
+  - LibraryService.java
+- tests
+  - LibraryServiceTest.java
+- userInterface
+  - UserOperatons.java
+- utilities
+  - Utility.java
+- Application.java
 
-- File structure refactor
-- Use a better ID value for books and users
-- Create Operations class for all operations
+## Next task:
+
+- Users to be able borrow movie DVDs
+
+## To be done:
+
 - Refactor the "display" methods
 - Read books from Datasource(file)
 - Read Users from Datasource(file)
-- Add tests
 - Handle exceptions
-
-- Naming
-- Separation of concern
-- Domain objects
